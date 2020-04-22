faa9049be16debdb7b661ea4910067c265f6af05
WolfyScript <wolfyscript@gmail.com>
2020-04-22 21:56:50 +0200

    Deprecated getId and getNamespace and replaced it with getNamespacedKey

--- src/main/java/me/wolfyscript/utilities/api/custom_items/CustomConfig.java
+++ src/main/java/me/wolfyscript/utilities/api/custom_items/CustomConfig.java
@@ -2,17 +2,20 @@ package me.wolfyscript.utilities.api.custom_items;
 
 import me.wolfyscript.utilities.api.config.ConfigAPI;
 import me.wolfyscript.utilities.api.config.JsonConfiguration;
+import me.wolfyscript.utilities.api.utils.NamespacedKey;
 import org.bukkit.Material;
 
 public class CustomConfig extends JsonConfiguration {
 
     private String namespace;
     private String id;
+    private NamespacedKey namespacedKey;
 
 
     public CustomConfig(ConfigAPI configAPI, String namespace, String name, String path, String defaultPath, String defaultName, boolean override) {
         super(configAPI, path, name, defaultPath, defaultName, override);
         this.namespace = namespace;
+        this.namespacedKey = new NamespacedKey(namespace, name);
         this.id = namespace + ":" + name;
         setPathSeparator('.');
     }
@@ -20,6 +23,7 @@ public class CustomConfig extends JsonConfiguration {
     public CustomConfig(String jsonData, ConfigAPI configAPI, String namespace, String name, String defaultPath, String defaultName) {
         super(jsonData, configAPI, name, defaultPath, defaultName);
         this.namespace = namespace;
+        this.namespacedKey = new NamespacedKey(namespace, name);
         this.id = namespace + ":" + name;
         setPathSeparator('.');
     }
@@ -27,6 +31,7 @@ public class CustomConfig extends JsonConfiguration {
     public CustomConfig(ConfigAPI configAPI, String namespace, String name, String defaultPath, String defaultName) {
         super(configAPI, name, defaultPath, defaultName);
         this.namespace = namespace;
+        this.namespacedKey = new NamespacedKey(namespace, name);
         this.id = namespace + ":" + name;
         setPathSeparator('.');
     }
@@ -40,10 +45,16 @@ public class CustomConfig extends JsonConfiguration {
         this(configAPI, namespace, name, path, defaultPath, defaultName, override);
     }
 
+    public NamespacedKey getNamespacedKey() {
+        return namespacedKey;
+    }
+
+    @Deprecated
     public String getNamespace() {
         return namespace;
     }
 
+    @Deprecated
     public String getId() {
         return id;
     }
