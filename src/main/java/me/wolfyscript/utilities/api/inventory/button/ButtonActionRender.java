2f389db3dfd0e2d4b635f6ed9b1837d504e394d0
WolfyScript <wolfyscript@gmail.com>
2020-09-12 12:09:34 +0200

    Made ButtonActionRender backwards compatible and is now Deprecated

--- src/main/java/me/wolfyscript/utilities/api/inventory/button/ButtonActionRender.java
+++ src/main/java/me/wolfyscript/utilities/api/inventory/button/ButtonActionRender.java
@@ -1,4 +1,13 @@
 package me.wolfyscript.utilities.api.inventory.button;
 
-public interface ButtonActionRender extends ButtonAction, ButtonRender {
+import me.wolfyscript.utilities.api.inventory.GuiHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+
+@Deprecated
+public interface ButtonActionRender extends ButtonAction {
+
+    ItemStack render(HashMap<String, Object> values, GuiHandler guiHandler, Player player, ItemStack icon, int slot, boolean helpEnabled);
 }
