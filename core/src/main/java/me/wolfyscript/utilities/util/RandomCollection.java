c43b01eae46b4b259665f9ae1ca3a4b4799a12d0
WolfyScript <wolfyscript@gmail.com>
2021-03-24 09:08:13 +0100

    RandomCollection improvements & Bug Fixes It now extends TreeMap to open up more methods to values etc. RandomCollection#next now returns null if no value is available.

--- core/src/main/java/me/wolfyscript/utilities/util/RandomCollection.java
+++ core/src/main/java/me/wolfyscript/utilities/util/RandomCollection.java
@@ -1,14 +1,15 @@
 package me.wolfyscript.utilities.util;
 
-import java.util.NavigableMap;
+import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
+
+import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
 import java.util.function.BiConsumer;
 import java.util.stream.Collector;
 
-public class RandomCollection<E> {
+public class RandomCollection<E> extends TreeMap<Double, E> {
 
-    private final NavigableMap<Double, E> map = new TreeMap<Double, E>();
     private final Random random;
     private double total = 0;
 
@@ -20,32 +21,34 @@ public class RandomCollection<E> {
         this.random = random;
     }
 
-    public RandomCollection<E> add(double weight, E result) {
-        if (weight <= 0) return this;
-        total += weight;
-        map.put(total, result);
-        return this;
+    public static Collector<CustomItem, RandomCollection<CustomItem>, RandomCollection<CustomItem>> getCustomItemCollector() {
+        return Collector.of(RandomCollection::new, (rdmCollection, customItem) -> rdmCollection.add(customItem.getRarityPercentage(), customItem.clone()), RandomCollection::addAll);
     }
 
     public static <T> Collector<T, RandomCollection<T>, RandomCollection<T>> getCollector(BiConsumer<RandomCollection<T>, T> accumulator) {
         return Collector.of(RandomCollection::new, accumulator, RandomCollection::addAll);
     }
 
+    public RandomCollection<E> add(double weight, E result) {
+        if (weight <= 0) return this;
+        total += weight;
+        put(total, result);
+        return this;
+    }
+
+    /**
+     * Gets the next value in the map depending on the weight.
+     *
+     * @return The next random value or null if none can be found.
+     */
     public E next() {
         double value = random.nextDouble() * total;
-        return map.higherEntry(value).getValue();
-    }
-
-    public boolean isEmpty() {
-        return map.isEmpty();
-    }
-
-    public int size() {
-        return map.size();
+        Map.Entry<Double, E> entry = higherEntry(value);
+        return entry == null ? null : entry.getValue();
     }
 
     public RandomCollection<E> addAll(RandomCollection<E> randomCollection) {
-        map.putAll(randomCollection.map);
+        putAll(randomCollection);
         return this;
     }
 }
