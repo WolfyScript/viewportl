952d90f4253b173ca8ca09bdca8e0f365eba4da6
WolfyScript <wolfyscript@gmail.com>
2021-04-17 19:02:20 +0200

    Added RandomUtils#nextDouble(double, double) and #nextLong(long, long)

--- core/src/main/java/me/wolfyscript/utilities/util/RandomUtils.java
+++ core/src/main/java/me/wolfyscript/utilities/util/RandomUtils.java
@@ -7,6 +7,9 @@ import java.util.Random;
 
 public class RandomUtils {
 
+    private RandomUtils() {
+    }
+
     public static final Random random = new Random(System.currentTimeMillis());
 
     public static Vector getRandomVector() {
@@ -21,5 +24,33 @@ public class RandomUtils {
         double x = Math.cos(rnd);
         double z = Math.sin(rnd);
         return new Vector(x, 0.0D, z);
+    }
+
+    public static double nextDouble(double origin, double bound) {
+        double r = random.nextDouble();
+        r = r * (bound - origin) + origin;
+        if (r >= bound) // correct for rounding
+            r = Math.nextDown(bound);
+        return r;
+    }
+
+    public static long nextLong(long origin, long bound) {
+        long r = random.nextLong();
+        if (origin < bound) {
+            long n = bound - origin, m = n - 1;
+            if ((n & m) == 0L)  // power of two
+                r = (r & m) + origin;
+            else if (n > 0L) {  // reject over-represented candidates
+                for (long u = r >>> 1;            // ensure nonnegative
+                     u + m - (r = u % n) < 0L;    // rejection check
+                     u = random.nextLong() >>> 1) // retry
+                    ;
+                r += origin;
+            } else {              // range not representable as long
+                while (r < origin || r >= bound)
+                    r = random.nextLong();
+            }
+        }
+        return r;
     }
 }
