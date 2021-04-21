a86eea50429d8635c819ab757c8d51cfacb01812
WolfyScript <wolfyscript@gmail.com>
2021-04-21 21:09:11 +0200

    Removed next Double & Long because they are available via ThreadLocalRandom.

--- core/src/main/java/me/wolfyscript/utilities/util/RandomUtils.java
+++ core/src/main/java/me/wolfyscript/utilities/util/RandomUtils.java
@@ -25,32 +25,4 @@ public class RandomUtils {
         double z = Math.sin(rnd);
         return new Vector(x, 0.0D, z);
     }
-
-    public static double nextDouble(double origin, double bound) {
-        double r = random.nextDouble();
-        r = r * (bound - origin) + origin;
-        if (r >= bound) // correct for rounding
-            r = Math.nextDown(bound);
-        return r;
-    }
-
-    public static long nextLong(long origin, long bound) {
-        long r = random.nextLong();
-        if (origin < bound) {
-            long n = bound - origin, m = n - 1;
-            if ((n & m) == 0L)  // power of two
-                r = (r & m) + origin;
-            else if (n > 0L) {  // reject over-represented candidates
-                for (long u = r >>> 1;            // ensure nonnegative
-                     u + m - (r = u % n) < 0L;    // rejection check
-                     u = random.nextLong() >>> 1) // retry
-                    ;
-                r += origin;
-            } else {              // range not representable as long
-                while (r < origin || r >= bound)
-                    r = random.nextLong();
-            }
-        }
-        return r;
-    }
 }
