35f25e0782150235dceb0b18514866346839071e
WolfyScript <wolfyscript@gmail.com>
2021-06-09 18:51:25 +0200

    Improved CustomDurabilityListener

--- wolfyutilities/src/main/java/me/wolfyscript/utilities/main/listeners/custom_item/CustomDurabilityListener.java
+++ wolfyutilities/src/main/java/me/wolfyscript/utilities/main/listeners/custom_item/CustomDurabilityListener.java
@@ -8,15 +8,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.event.player.PlayerItemMendEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class CustomDurabilityListener implements Listener {
 
     @EventHandler
     public void onDamage(PlayerItemDamageEvent event) {
-        CustomItem customItem = new CustomItem(event.getItem());
-        ItemStack itemStack = event.getItem();
-
+        var customItem = new CustomItem(event.getItem());
+        var itemStack = event.getItem();
         if (customItem.hasCustomDurability()) {
             event.setCancelled(true);
             int totalDmg = customItem.getCustomDamage() + event.getDamage();
@@ -31,14 +29,10 @@ public class CustomDurabilityListener implements Listener {
 
     @EventHandler
     public void onMend(PlayerItemMendEvent event) {
-        CustomItem customItem = new CustomItem(event.getItem());
+        var customItem = new CustomItem(event.getItem());
         if (customItem.hasCustomDurability()) {
             int repairAmount = Math.min(event.getExperienceOrb().getExperience() * 2, customItem.getCustomDamage());
-            int totalDmg = customItem.getCustomDamage() - repairAmount;
-            if (!(totalDmg >= 0)) {
-                totalDmg = 0;
-            }
-            int finalTotalDmg = totalDmg;
+            int finalTotalDmg = Math.max(0, customItem.getCustomDamage() - repairAmount);
             Bukkit.getScheduler().runTask(WUPlugin.getInstance(), () -> customItem.setCustomDamage(finalTotalDmg));
         }
     }
