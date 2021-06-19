ee6da004d5a6086aff210987d671313d701aa4be
WolfyScript <wolfyscript@gmail.com>
2021-06-19 16:23:52 +0200

    Added CustomTypeResolver and CustomTypeIdResolver. They make it easier to serialize/deserialize custom Keyed objects.

---
+++ core/src/main/java/me/wolfyscript/utilities/util/json/jackson/CustomTypeIdResolver.java
@@ -0,0 +1,62 @@
+package me.wolfyscript.utilities.util.json.jackson;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DatabindContext;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import me.wolfyscript.utilities.util.Keyed;
+import me.wolfyscript.utilities.util.NamespacedKey;
+import me.wolfyscript.utilities.util.Registry;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomTypeIdResolver extends TypeIdResolverBase {
+
+    private static final Map<Class<?>, Registry<?>> TYPE_REGISTRIES = new HashMap<>();
+
+    public static <T extends Keyed> void registerTypeRegistry(Class<T> type, Registry<T> registry) {
+        if (!TYPE_REGISTRIES.containsKey(type)) {
+            TYPE_REGISTRIES.put(type, registry);
+        }
+    }
+
+    @Override
+    public String idFromValue(Object value) {
+        return getKey(value);
+    }
+
+    @Override
+    public String idFromValueAndType(Object value, Class<?> aClass) {
+        return getKey(value);
+    }
+
+    private String getKey(Object value) {
+        if (value instanceof Keyed keyed) {
+            return keyed.getNamespacedKey().toString();
+        }
+        throw new IllegalArgumentException(String.format("Object %s is not of type Keyed!", value.getClass().getName()));
+    }
+
+    @Override
+    public JavaType typeFromId(DatabindContext context, String id) throws IOException {
+        Registry<?> registry = TYPE_REGISTRIES.get(_baseType.getRawClass());
+        if (registry != null) {
+            var namespacedKey = NamespacedKey.of(id);
+            if (namespacedKey != null) {
+                var object = registry.get(namespacedKey);
+                if (object != null) {
+                    return context.constructSpecializedType(TypeFactory.unknownType(), object.getClass());
+                }
+            }
+        }
+        return TypeFactory.unknownType();
+    }
+
+    @Override
+    public JsonTypeInfo.Id getMechanism() {
+        return JsonTypeInfo.Id.CUSTOM;
+    }
+}
