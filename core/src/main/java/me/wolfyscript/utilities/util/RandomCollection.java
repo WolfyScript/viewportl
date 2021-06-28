53ea9d522a8807cd80f6ca6e9975587d2e0f60c7
WolfyScript <wolfyscript@gmail.com>
2021-06-28 13:43:17 +0200

    RandomCollection will use ThreadLocalRandom whenever possible. It can be overridden by using RandomCollection(Random) or RandomCollection#next(Random).

--- core/src/main/java/me/wolfyscript/utilities/util/RandomCollection.java
+++ core/src/main/java/me/wolfyscript/utilities/util/RandomCollection.java
@@ -3,21 +3,26 @@ package me.wolfyscript.utilities.util;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.BiConsumer;
 import java.util.stream.Collector;
 
 public class RandomCollection<E> extends TreeMap<Double, E> {
 
-    private final Random random;
     private double total = 0;
+    private final Random random;
 
     public RandomCollection() {
-        this(new Random());
+        this.random = null;
     }
 
+    /**
+     * When using this constructor the specified Random will be used whenever an item is selected.
+     *
+     * @param random The random to use when selecting an item.
+     */
     public RandomCollection(Random random) {
         this.random = random;
     }
@@ -44,9 +49,18 @@ public class RandomCollection<E> extends TreeMap<Double, E> {
      */
     @Nullable
     public E next() {
+        return next(random != null ? random : ThreadLocalRandom.current());
+    }
+
+    /**
+     * Gets the next value in the map depending on the weight.
+     *
+     * @return The next random value or null if none can be found.
+     */
+    @Nullable
+    public E next(Random random) {
         double value = random.nextDouble() * total;
-        Map.Entry<Double, E> entry = higherEntry(value);
-        return entry == null ? null : entry.getValue();
+        return ceilingEntry(value).getValue();
     }
 
     public RandomCollection<E> addAll(RandomCollection<E> randomCollection) {
