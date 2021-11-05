24f686d705e801b9c917f11e21f30604ab7067cd
WolfyScript <wolfyscript@gmail.com>
2021-11-05 20:10:52 +0100

    Updated MetaSettings to convert old data and load the new data format.

--- core/src/main/java/me/wolfyscript/utilities/api/inventory/custom_items/meta/MetaSettings.java
+++ core/src/main/java/me/wolfyscript/utilities/api/inventory/custom_items/meta/MetaSettings.java
@@ -18,56 +18,80 @@
 
 package me.wolfyscript.utilities.api.inventory.custom_items.meta;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Preconditions;
 import me.wolfyscript.utilities.util.NamespacedKey;
 import me.wolfyscript.utilities.util.Registry;
 import me.wolfyscript.utilities.util.inventory.item_builder.ItemBuilder;
 import me.wolfyscript.utilities.util.json.jackson.JacksonUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 
 @JsonDeserialize(using = MetaSettings.Deserializer.class)
 public class MetaSettings extends HashMap<NamespacedKey, Meta> {
 
-    private final Set<Meta> checks = new HashSet<>();
+    private static final String CHECKS_KEY = "checks";
 
+    private final List<Meta> checks;
+
+    /**
+     * Creates a new settings object with an empty list of checks.
+     */
     public MetaSettings() {
-        Registry.META_PROVIDER.entrySet().forEach(entry -> put(entry.getKey(), entry.getValue().provide()));
+        checks = new ArrayList<>();
     }
 
-    public MetaSettings(JsonNode jsonNode) {
-        this();
+    /**
+     * This constructor is only used for serializing the object from a JsonNode.
+     * It will convert the old data to the new system if necessary.
+     *
+     * @param jsonNode The {@link JsonNode} to load the data from.
+     */
+    private MetaSettings(JsonNode jsonNode) {
         if (jsonNode instanceof ObjectNode node) {
-            //TODO: Replace Meta.Provider with the new KeyedTypeIdResolver
-            if (!node.has("checks")) {
+            if (!node.has(CHECKS_KEY)) {
+                checks = new ArrayList<>();
+                //Convert old meta to new format.
                 node.fields().forEachRemaining(entry -> {
-                    String key = entry.getKey().toLowerCase(Locale.ROOT);
-                    NamespacedKey namespacedKey = key.contains(":") ? NamespacedKey.of(key) : NamespacedKey.wolfyutilties(key);
-                    Meta.Provider<?> provider = Registry.META_PROVIDER.get(namespacedKey);
-                    if (provider != null) {
-                        Meta meta;
-                        if (entry.getValue().isTextual()) {
-                            meta = provider.provide();
-                            meta.setOption(JacksonUtil.getObjectMapper().convertValue(entry.getValue(), MetaSettings.Option.class));
-                        } else {
-                            meta = provider.parse(entry.getValue());
+                    if(entry.getValue() instanceof ObjectNode value) {
+                        String key = entry.getKey().toLowerCase(Locale.ROOT);
+                        NamespacedKey namespacedKey = key.contains(":") ? NamespacedKey.of(key) : NamespacedKey.wolfyutilties(key);
+                        if (namespacedKey != null) {
+                            value.put("key", String.valueOf(namespacedKey));
+                            Meta meta = JacksonUtil.getObjectMapper().convertValue(value, Meta.class);
+                            if (meta != null && !meta.getOption().equals(Option.IGNORE)) {
+                                checks.add(meta);
+                            }
                         }
-                        put(namespacedKey, meta);
                     }
                 });
             } else {
-
-
+                JsonNode checksNode = node.get(CHECKS_KEY);
+                checks = JacksonUtil.getObjectMapper().convertValue(checksNode, new TypeReference<>() {});
             }
+        } else {
+            checks = new ArrayList<>();
         }
+    }
+
+    /**
+     * @param meta The {@link Meta} to add to the list of checks. Cannot be null, and must not have the {@link Option#IGNORE}, or it will throw an exception!
+     */
+    public void addCheck(@NotNull Meta meta) {
+        Objects.requireNonNull(meta, "Meta check cannot be null!");
+        Preconditions.checkArgument(!meta.getOption().equals(Option.IGNORE), "Deprecated option! Ignored check cannot be added!");
+        checks.add(meta);
+    }
+
+    public List<Meta> getChecks() {
+        return List.copyOf(checks);
     }
 
     public boolean check(ItemBuilder itemOther, ItemBuilder item) {
@@ -75,7 +99,17 @@ public class MetaSettings extends HashMap<NamespacedKey, Meta> {
     }
 
     public enum Option {
-        EXACT, @Deprecated(forRemoval = true) IGNORE, HIGHER, HIGHER_EXACT, LOWER, LOWER_EXACT, HIGHER_LOWER
+        EXACT,
+        /**
+         * This option was originally used to indicate if the meta check should be active.
+         * Now it is no longer used (Only to convert old data), because checks that are not added to the settings are well... not checked anyway.
+         */
+        @Deprecated(forRemoval = true) IGNORE,
+        HIGHER,
+        HIGHER_EXACT,
+        LOWER,
+        LOWER_EXACT,
+        HIGHER_LOWER
     }
 
     public static class Deserializer extends StdDeserializer<MetaSettings> {
