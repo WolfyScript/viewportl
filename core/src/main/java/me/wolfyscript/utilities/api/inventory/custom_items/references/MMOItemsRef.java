006fe84ca515abd2ace48e6153dde7332e27a6fa
WolfyScript <wolfyscript@gmail.com>
2021-11-12 23:02:20 +0100

    Fixed MMOItems incompatibility by building the items without the ItemBuildEvent.

--- core/src/main/java/me/wolfyscript/utilities/api/inventory/custom_items/references/MMOItemsRef.java
+++ core/src/main/java/me/wolfyscript/utilities/api/inventory/custom_items/references/MMOItemsRef.java
@@ -23,7 +23,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import io.lumine.mythic.lib.api.item.NBTItem;
 import net.Indyuce.mmoitems.MMOItems;
+import net.Indyuce.mmoitems.api.MMOItemsAPI;
 import net.Indyuce.mmoitems.api.Type;
+import net.Indyuce.mmoitems.api.item.mmoitem.MMOItem;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
@@ -52,7 +54,8 @@ public class MMOItemsRef extends APIReference {
 
     @Override
     public ItemStack getLinkedItem() {
-        return MMOItems.plugin.getItem(itemType, itemName);
+        MMOItem item = MMOItems.plugin.getMMOItem(itemType, itemName);
+        return item != null ? item.newBuilder().buildSilently() : null;
     }
 
     @Override
