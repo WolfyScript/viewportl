f4b22dc3cef2a330c8c8809291f6227b1ab9a173
WolfyScript <wolfyscript@gmail.com>
2021-12-23 00:34:08 +0100

    Added simple rotation to AnimatorCircle

--- core/src/main/java/me/wolfyscript/utilities/util/particles/animators/AnimatorCircle.java
+++ core/src/main/java/me/wolfyscript/utilities/util/particles/animators/AnimatorCircle.java
@@ -18,11 +18,15 @@
 
 package me.wolfyscript.utilities.util.particles.animators;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import me.wolfyscript.utilities.util.NamespacedKey;
 import me.wolfyscript.utilities.util.particles.ParticleEffect;
 import me.wolfyscript.utilities.util.particles.timer.Timer;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -33,14 +37,35 @@ public class AnimatorCircle extends Animator {
     public static final NamespacedKey KEY = NamespacedKey.wolfyutilties("circle");
 
     private final int radius;
+    private Vector angleDeg;
+
+    @JsonIgnore
+    private Vector angleRad;
+    @JsonIgnore
+    private double xAxisCos; // getting the cos value for the pitch.
+    @JsonIgnore
+    private double xAxisSin; // getting the sin value for the pitch.
+    @JsonIgnore
+    private double yAxisCos; // getting the cos value for the yaw.
+    @JsonIgnore
+    private double yAxisSin; // getting the sin value for the yaw.
+    @JsonIgnore
+    private double zAxisCos; // getting the cos value for the roll.
+    @JsonIgnore
+    private double zAxisSin; // getting the sin value for the roll.
 
     public AnimatorCircle() {
         this(1);
     }
 
     public AnimatorCircle(int radius) {
+        this(radius, new Vector(0, 0, 0));
+    }
+
+    public AnimatorCircle(int radius, Vector angleDeg) {
         super(KEY);
         this.radius = radius;
+        setAngleDeg(angleDeg);
     }
 
     @Override
@@ -48,9 +73,32 @@ public class AnimatorCircle extends Animator {
         double time = timer.increase();
         double x = radius * Math.cos(time);
         double z = radius * Math.sin(time);
-        origin.add(x, 0, z);
+
+        Vector vec = new Vector(x, 0, z);
+        rotateAroundAxisX(vec, xAxisCos, xAxisSin);
+        rotateAroundAxisY(vec, yAxisCos, yAxisSin);
+        rotateAroundAxisZ(vec, zAxisCos, zAxisSin);
+
+        origin.add(vec);
         spawnParticle(effect, origin, player);
-        origin.subtract(x, 0, z);
+        origin.subtract(vec);
+    }
+
+    @JsonSetter("angle")
+    private void setAngleDeg(Vector angleDeg) {
+        this.angleDeg = angleDeg;
+        this.angleRad = new Vector(Math.toRadians(this.angleDeg.getX()), Math.toRadians(this.angleDeg.getY()), Math.toRadians(this.angleDeg.getZ()));
+        this.xAxisCos = Math.cos(angleRad.getX()); // getting the cos value for the pitch.
+        this.xAxisSin = Math.sin(angleRad.getX()); // getting the sin value for the pitch.
+        this.yAxisCos = Math.cos(-angleRad.getY()); // getting the cos value for the yaw.
+        this.yAxisSin = Math.sin(-angleRad.getY()); // getting the sin value for the yaw.
+        this.zAxisCos = Math.cos(angleRad.getZ()); // getting the cos value for the roll.
+        this.zAxisSin = Math.sin(angleRad.getZ()); // getting the sin value for the roll.
+    }
+
+    @JsonGetter("angle")
+    private Vector getAngleDeg() {
+        return angleDeg;
     }
 
     @Override
