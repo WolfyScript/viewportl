a0434c1725236c62c47608c864b003d134b6055d
WolfyScript <wolfyscript@gmail.com>
2022-01-06 19:06:54 +0100

    Added more arguments to ResourceLoader#load

--- core/src/main/java/me/wolfyscript/utilities/expansions/ExpansionPack.java
+++ core/src/main/java/me/wolfyscript/utilities/expansions/ExpansionPack.java
@@ -21,6 +21,7 @@ package me.wolfyscript.utilities.expansions;
 import me.wolfyscript.utilities.api.WolfyUtilCore;
 import me.wolfyscript.utilities.registry.RegistryResourceLoader;
 import me.wolfyscript.utilities.util.NamespacedKey;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,15 +33,17 @@ public class ExpansionPack {
     static final int VERSION = 1;
 
     private final WolfyUtilCore core;
+    private final PackMetaFile meta;
     private final File file;
     private final List<String> zipEntryNames;
     private final String rootDir;
 
-    ExpansionPack(File file, List<String> entryNames, WolfyUtilCore core) {
+    ExpansionPack(@NotNull PackMetaFile meta, File file, List<String> entryNames, WolfyUtilCore core) {
         this.file = file;
         this.core = core;
         this.zipEntryNames = entryNames;
         this.rootDir = entryNames.get(0);
+        this.meta = meta;
     }
 
     public void load(RegistryResourceLoader registry, List<NamespacedKey> loaderOrder) throws IOException {
@@ -49,12 +52,13 @@ public class ExpansionPack {
             for (NamespacedKey namespacedKey : loaderOrder) {
                 var loader = registry.get(namespacedKey);
                 if (loader != null) {
-                    String loaderRoot = rootDir + namespacedKey.getNamespace() + "/" + namespacedKey.getKey() + "/"; //Get the root of the loader
+                    String loaderRoot = rootDir + loader.folderPath; //Get the root of the loader
                     int rootIndex = zipEntryNames.indexOf(loaderRoot); // Get the index of that root in the zip file.
                     if (rootIndex == -1) {
                         continue; // The loader root is not in the zip file.
                     }
-                    for (int i = rootIndex; i < zipEntryNames.size(); i++) {
+                    //We start one index later, as the first one is always the root folder
+                    for (int i = rootIndex + 1; i < zipEntryNames.size(); i++) {
                         String entryName = zipEntryNames.get(i);
                         if (!entryName.startsWith(loaderRoot)) {
                             break;
@@ -62,7 +66,12 @@ public class ExpansionPack {
                         var entry = zipFile.getEntry(entryName);
                         if (entry != null) {
                             // Call the loader specific load method
-                            loader.load(entry, core);
+                            try {
+                                loader.load(this, zipFile, rootDir, entry, core);
+                            } catch (Exception e) {
+                                // We don't want a faulty loader to crash the whole loading process and/or plugin!
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }
@@ -70,7 +79,23 @@ public class ExpansionPack {
         }
     }
 
+    public PackMetaFile getMeta() {
+        return meta;
+    }
+
     public File getFile() {
         return file;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExpansionPack pack)) return false;
+        return meta.getPack().getNamespace().equals(pack.meta.getPack().getNamespace());
+    }
+
+    @Override
+    public int hashCode() {
+        return meta.getPack().getNamespace().hashCode();
     }
 }
