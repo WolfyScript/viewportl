bc0e027441e2332e33b6ce76afc53adc463c989b
WolfyScript <wolfyscript@gmail.com>
2022-02-15 17:12:54 +0100

    Language no longer makes use of the JsonConfig (It only extends it for backwards compatibility). It is fully serializable using Jackson with JacksonInjects

--- core/src/main/java/me/wolfyscript/utilities/api/language/Language.java
+++ core/src/main/java/me/wolfyscript/utilities/api/language/Language.java
@@ -19,11 +19,14 @@
 package me.wolfyscript.utilities.api.language;
 
 import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.Streams;
 import me.wolfyscript.utilities.api.WolfyUtilCore;
 import me.wolfyscript.utilities.api.WolfyUtilities;
 import me.wolfyscript.utilities.api.config.JsonConfig;
@@ -35,46 +38,101 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Language extends JsonConfig<Language> {
+/**
+ * Represents a single Language that can be loaded from a JSON file.<br>
+ * You can load and register a language using {@link LanguageAPI#loadLangFile(String)}
+ *
+ * <p>
+ * <strong>It only extends the {@link JsonConfig} for backwards compatibility, so in future it will get removed!</strong><br>
+ * Instead of creating it via the old constructor you need to use the {@link LanguageAPI#loadLangFile(String)} method!
+ * </p>
+ */
+public class Language extends JsonConfig<JsonNode> {
 
+    @JsonIgnore
     private final WolfyUtilities api;
+    @JsonIgnore
     private final String lang;
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     private final Type type = Type.NESTED;
+    @JsonIgnore
     private final Map<String, LanguageNode> mappedLangNodes = new HashMap<>();
 
+    /**
+     * Used to create (deserialize) the Language from Json. The {@link LanguageAPI} provides the option to load the Language from a file.
+     *
+     * @param file The file of the language (Only necessary for backwards compatibility!)
+     * @param api The api this Language belongs to.
+     * @param lang The language name.
+     * @see LanguageAPI#loadLangFile(String)
+     */
     @JsonCreator
-    public Language(@JsonProperty("api") @JacksonInject("api")  WolfyUtilities api, @JsonProperty("lang") @JacksonInject("lang") String lang) {
-        super(new File(api.getPlugin().getDataFolder(), "lang/" + lang + ".json"), Language.class);
+    protected Language(@JsonProperty("file") @JacksonInject File file, @JsonProperty("api") @JacksonInject WolfyUtilities api, @JsonProperty("lang") @JacksonInject String lang) {
+        super(file, JsonNode.class);
         this.lang = lang;
         this.api = api;
     }
 
+    /**
+     * Creates a new Language and loads it from the file. <br>
+     * <strong>Only used for backwards compatibility!</strong>
+     *
+     * @deprecated {@link LanguageAPI#loadLangFile(String)} should be used instead!
+     * @param plugin
+     * @param lang
+     */
+    @Deprecated
     public Language(Plugin plugin, String lang) {
-        super(new File(plugin.getDataFolder(), "lang/" + lang + ".json"), Language.class);
+        super(new File(plugin.getDataFolder(), "lang/" + lang + ".json"), JsonNode.class);
         this.lang = lang;
         this.api = WolfyUtilCore.getInstance().getAPI(plugin);
+        //Make sure Languages created the old way are still setting the values.
+        setValues(Streams.stream(value.fields()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
     }
 
     @JsonAnySetter
     private void setValues(Map<String, JsonNode> nodes) {
-        if (type.equals(Type.NESTED)) {
-            nodes.forEach((fieldName, jsonNode) -> readNestedNode(new StringBuilder(fieldName), jsonNode));
-        } else {
-            //Support for flatten languages
+        switch (type) {
+            case NESTED -> nodes.forEach(this::readNestedNode);
+            case FLAT -> nodes.forEach((path, jsonNode) -> {
+                if (jsonNode.isArray()) {
+                    mappedLangNodes.put(path, new LanguageNodeArray(api.getChat(), jsonNode));
+                } else {
+                    mappedLangNodes.put(path, new LanguageNodeText(api.getChat(), jsonNode));
+                }
+            });
         }
     }
 
-    private void readNestedNode(StringBuilder path, JsonNode node) {
+    /**
+     * Always saves the language in a semi-flat style.
+     * In the {@link Type#FLAT} style.
+     *
+     * @return The semi-flat representation of the language.
+     */
+    @JsonAnyGetter
+    private Map<String, LanguageNode> getValues() {
+        mappedLangNodes.values().removeIf(LanguageNodeMissing.class::isInstance);
+        return mappedLangNodes;
+    }
+
+    private void readNestedNode(String path, JsonNode node) {
         if (node.isObject()) {
-            node.fields().forEachRemaining(entry -> readNestedNode(path.append(".").append(entry.getKey()), entry.getValue()));
+            node.fields().forEachRemaining(entry -> readNestedNode(path + "." + entry.getKey(), entry.getValue()));
         } else if (node.isArray()) {
-            mappedLangNodes.put(path.toString(), new LanguageNodeArray(api.getChat(), node));
+            mappedLangNodes.put(path, new LanguageNodeArray(api.getChat(), node));
         } else {
-            mappedLangNodes.put(path.toString(), new LanguageNodeText(api.getChat(), node));
+            mappedLangNodes.put(path, new LanguageNodeText(api.getChat(), node));
         }
     }
 
+    /**
+     * Gets the name of this language.
+     *
+     * @return The name of the language.
+     */
     @JsonIgnore
     public String getName() {
         return lang;
@@ -107,8 +165,40 @@ public class Language extends JsonConfig<Language> {
         return getNode(path).getValue();
     }
 
+    /**
+     * The type in which the language data is provided.
+     */
     public enum Type {
+        /**
+         * Is the normal JSON format you are used to.
+         * <pre>
+         *  {
+         *      "an" : {
+         *          "nested" : {
+         *              "value": 2,
+         *              "array" : [
+         *                  "element",
+         *                  "element1",
+         *                  "element2"
+         *              ]
+         *          }
+         *      }
+         *  }</pre>
+         */
         NESTED,
+        /**
+         * Flattens every object using the '.' path separator.
+         * Arrays are kept as is and not split into separate fields.
+         * <pre>
+         *  {
+         *      "an.nested.value": 2,
+         *      "an.nested.array" : [
+         *          "element",
+         *          "element1",
+         *          "element2"
+         *      ]
+         *  }</pre>
+         */
         FLAT
 
     }
