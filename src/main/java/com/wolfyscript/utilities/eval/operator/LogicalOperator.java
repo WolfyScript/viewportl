85bcf0afd5fbe6febda7763f0c565382cefbfcdd
WolfyScript <wolfyscript@gmail.com>
2022-11-19 23:51:59 +0100

    Moved all Operators to this repo * Now all these classes require WolfyUtils when deserialized, because of the changes to NamespacedKey construction.

---
+++ src/main/java/com/wolfyscript/utilities/eval/operator/LogicalOperator.java
@@ -0,0 +1,46 @@
+/*
+ *       WolfyUtilities, APIs and Utilities for Minecraft Spigot plugins
+ *                      Copyright (C) 2021  WolfyScript
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.wolfyscript.utilities.eval.operator;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wolfyscript.utilities.common.WolfyUtils;
+
+/**
+ * An Operator that represents logical operators like and (&&), or (||), not (!).<br>
+ * They evaluate at least one inner {@link BoolOperator}, which then results in a booleanish output.
+ *
+ * <ul>
+ *     <li>{@link LogicalOperatorAnd}</li>
+ *     <li>{@link LogicalOperatorOr}</li>
+ *     <li>{@link LogicalOperatorNot}</li>
+ * </ul>
+ *
+ */
+public abstract class LogicalOperator extends BoolOperator {
+
+    @JsonProperty("this")
+    protected final BoolOperator thisValue;
+
+    public LogicalOperator(@JacksonInject WolfyUtils wolfyUtils, BoolOperator thisValue) {
+        super(wolfyUtils);
+        this.thisValue = thisValue;
+    }
+
+}
