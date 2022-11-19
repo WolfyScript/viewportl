12bd16f5d781f40f25e474654d9b25459b4b8f4f
WolfyScript <wolfyscript@gmail.com>
2022-11-19 23:47:41 +0100

    Moved Registry classes to this repo

---
+++ src/main/java/com/wolfyscript/utilities/registry/UniqueTypeRegistrySimple.java
@@ -0,0 +1,18 @@
+package com.wolfyscript.utilities.registry;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.wolfyscript.utilities.Keyed;
+import com.wolfyscript.utilities.NamespacedKey;
+
+public class UniqueTypeRegistrySimple<V extends Keyed> extends AbstractTypeRegistry<BiMap<NamespacedKey, Class<? extends V>>, V> {
+
+    public UniqueTypeRegistrySimple(NamespacedKey key, Registries registries) {
+        super(key, HashBiMap.create(), registries);
+    }
+
+    public NamespacedKey getKey(Class<? extends V> value) {
+        return map.inverse().get(value);
+    }
+
+}
