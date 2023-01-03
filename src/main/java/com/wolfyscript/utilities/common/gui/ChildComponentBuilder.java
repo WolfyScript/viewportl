857f1cb9ea3df605f95615f9561a78746256a3ca
WolfyScript <wolfyscript@gmail.com>
2023-01-03 19:42:52 +0100

    Added ChildComponentBuilder and Cluster and Window builders

---
+++ src/main/java/com/wolfyscript/utilities/common/gui/ChildComponentBuilder.java
@@ -0,0 +1,31 @@
+/*
+ *       WolfyUtilities, APIs and Utilities for Minecraft Spigot plugins
+ *                      Copyright (C) 2021  WolfyScript
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.wolfyscript.utilities.common.gui;
+
+import com.google.common.collect.BiMap;
+import com.wolfyscript.utilities.NamespacedKey;
+import java.util.function.Consumer;
+
+public interface ChildComponentBuilder<D extends Data> {
+
+    // TODO: This needs proper testing
+    <CT extends Component.Builder<D, ?, ?>> ChildComponentBuilder<D> custom(String subID, NamespacedKey builderId, Class<CT> builderType, Consumer<CT> builderConsumer);
+
+    BiMap<String, ? extends Component<D>> create();
+}
