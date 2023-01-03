0091e8cd33c3b51ef2ed13b8d3e859e00a814174
WolfyScript <wolfyscript@gmail.com>
2023-01-03 20:40:07 +0100

    Added default ButtonState implementation

---
+++ src/main/java/com/wolfyscript/utilities/common/gui/ComponentStateDefault.java
@@ -0,0 +1,124 @@
+/*
+ *       WolfyUtilities, APIs and Utilities for Minecraft Spigot plugins
+ *                      Copyright (C) 2021  WolfyScript
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.wolfyscript.utilities.common.gui;
+
+import com.google.common.base.Preconditions;
+import java.util.Objects;
+
+public class ComponentStateDefault<D extends Data> implements ComponentState<D> {
+
+        private final String key;
+        private final InteractionCallback<D> interactionCallback;
+        private final InteractionPostCallback<D> interactPostCallback;
+        private final RenderPreCallback<D> renderPreCallback;
+        private final RenderCallback<D> renderCallback;
+
+        protected ComponentStateDefault(String key, InteractionCallback<D> interactionCallback, InteractionPostCallback<D> interactPostCallback, RenderPreCallback<D> renderPreCallback, RenderCallback<D> renderCallback) {
+            this.key = key;
+            this.interactionCallback = interactionCallback;
+            this.interactPostCallback = interactPostCallback;
+            this.renderPreCallback = renderPreCallback;
+            this.renderCallback = renderCallback;
+        }
+
+        @Override
+        public InteractionCallback<D> interactCallback() {
+            return interactionCallback;
+        }
+
+        @Override
+        public InteractionPostCallback<D> interactPostCallback() {
+            return interactPostCallback;
+        }
+
+        @Override
+        public RenderCallback<D> renderCallback() {
+            return renderCallback;
+        }
+
+        @Override
+        public RenderPreCallback<D> renderPreCallback() {
+            return renderPreCallback;
+        }
+
+        @Override
+        public String key() {
+            return key;
+        }
+
+        public static class Builder<D extends Data> implements ComponentState.Builder<D, ComponentState<D>> {
+
+            private String key;
+            private InteractionCallback<D> interactionCallback;
+            private InteractionPostCallback<D> interactPostCallback;
+            private RenderPreCallback<D> renderPreCallback;
+            private RenderCallback<D> renderCallback;
+
+            protected Builder(String ownerID) {
+                this.key = ownerID;
+            }
+
+            @Override
+            public Builder<D> subKey(String subKey) {
+                this.key += "." + subKey;
+                return this;
+            }
+
+            @Override
+            public Builder<D> key(String key) {
+                this.key = key;
+                return this;
+            }
+
+            @Override
+            public Builder<D> interact(InteractionCallback<D> interactionCallback) {
+                this.interactionCallback = interactionCallback;
+                return this;
+            }
+
+            @Override
+            public Builder<D> interactPost(InteractionPostCallback<D> interactPostCallback) {
+                this.interactPostCallback = interactPostCallback;
+                return this;
+            }
+
+            @Override
+            public Builder<D> renderPre(RenderPreCallback<D> renderPreCallback) {
+                this.renderPreCallback = renderPreCallback;
+                return null;
+            }
+
+            @Override
+            public Builder<D> render(RenderCallback<D> renderCallback) {
+                this.renderCallback = renderCallback;
+                return null;
+            }
+
+            @Override
+            public ComponentStateDefault<D> create() {
+                Preconditions.checkNotNull(renderCallback, "Cannot create Component without a RenderCallback!");
+                final var interactCallback = Objects.requireNonNullElseGet(this.interactionCallback, () -> (holder, data, component, details) -> InteractionResult.def());
+                final var interactPostCallback = Objects.requireNonNullElseGet(this.interactPostCallback, () -> (holder, data, component, details) -> {});
+                final var renderPreCallback = Objects.requireNonNullElseGet(this.renderPreCallback, () -> (holder, data, component, context) -> {});
+                final var renderCallback = Objects.requireNonNullElseGet(this.renderCallback, () -> (holder, data, component, context) -> {});
+
+                return new ComponentStateDefault<>(key, interactCallback, interactPostCallback, renderPreCallback, renderCallback);
+            }
+        }
+    }
