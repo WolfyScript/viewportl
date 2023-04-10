0d111eac602e7749e461fa2ad68ccc042a7c2523
WolfyScript <wolfyscript@gmail.com>
2023-04-10 22:15:41 +0200

    Add ButtonBuilder interface to create Buttons

---
+++ src/main/java/com/wolfyscript/utilities/common/gui/ButtonBuilder.java
@@ -0,0 +1,48 @@
+/*
+ *       WolfyUtilities, APIs and Utilities for Minecraft Spigot plugins
+ *                      Copyright (C) 2021  WolfyScript
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.wolfyscript.utilities.common.gui;
+
+import com.wolfyscript.utilities.common.items.ItemStackConfig;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public interface ButtonBuilder {
+
+    ButtonBuilder icon(Consumer<IconBuilder> icon);
+
+    ButtonBuilder interact(InteractionCallback interactionCallback);
+
+    <T> ButtonBuilder useSignal(String key, Class<T> type, Consumer<Signal.Builder<T>> signalBuilder);
+
+    Button create(SizedComponent parent);
+
+    interface IconBuilder {
+
+        IconBuilder stack(ItemStackConfig<?> stackConfig);
+
+        IconBuilder stack(Supplier<ItemStackConfig<?>> stackConfigSupplier);
+
+        IconBuilder dynamic();
+
+        IconBuilder dynamic(boolean isDynamic);
+
+        ButtonIcon create();
+    }
+
+}
