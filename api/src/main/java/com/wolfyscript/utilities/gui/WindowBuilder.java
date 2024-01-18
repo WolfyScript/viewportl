2cd8a0b0a10fc5d9edb44e4e46da27e4081f03aa
WolfyScript <wolfyscript@gmail.com>
2024-01-18 20:04:01 +0100

    Add methods of WindowDynamicConstructor to WindowBuilder

--- api/src/main/java/com/wolfyscript/utilities/gui/WindowBuilder.java
+++ api/src/main/java/com/wolfyscript/utilities/gui/WindowBuilder.java
@@ -22,6 +22,10 @@ import com.wolfyscript.utilities.gui.callback.InteractionCallback;
 import com.wolfyscript.utilities.gui.functions.SerializableConsumer;
 import com.wolfyscript.utilities.config.jackson.KeyedBaseType;
 import java.util.function.Consumer;
+
+import com.wolfyscript.utilities.gui.functions.SerializableFunction;
+import com.wolfyscript.utilities.gui.functions.SerializableSupplier;
+import com.wolfyscript.utilities.gui.signal.Signal;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -65,12 +69,107 @@ public interface WindowBuilder {
     WindowBuilder interact(InteractionCallback interactionCallback);
 
     /**
-     * Specifies the constructor callback, that is called right before the component is created.
+     * Specify a dynamic custom title supplier that is used to update the title of the Inventory.<br>
+     * <p>
+     * Any signal used inside the supplier will cause it to update when the signal is updated.
+     * </p>
      *
-     * @param render The consumer to configure the renderer
+     * @param titleSupplier The supplier that provides the new title of the inventory
      * @return This builder for chaining
      */
-    WindowBuilder construct(Consumer<WindowDynamicConstructor> render);
+    WindowBuilder title(SerializableSupplier<net.kyori.adventure.text.Component> titleSupplier);
+
+    /**
+     * When no dynamic title is used (see {@link #title(SerializableSupplier)}), then this method can be used to
+     * create placeholder resolvers for the static title. The static title can be specified using {@link WindowBuilder#title(String)}.
+     * <p>
+     *     The title is updated whenever any of the specified signals are updated.
+     * </p>
+     *
+     * <p>
+     * The placeholder will be equal to the key of the signal.<br>
+     * e.g. <br>
+     * <pre>createSignal("count", () -> 0)</pre> will provide a placeholder <pre>&lt;count&gt;</pre>
+     *
+     * </p>
+     *
+     * @param signals The signals to use as placeholders
+     * @return This builder for chaining
+     */
+    WindowBuilder titleSignals(Signal<?>... signals);
+
+    /**
+     * Adds a task that is run periodically while the Window is open.
+     *
+     * @param runnable The task to run, may update signals
+     * @param intervalInTicks The interval for the task in ticks
+     * @return This builder for chaining
+     */
+    WindowBuilder addIntervalTask(Runnable runnable, long intervalInTicks);
+
+    /**
+     * <p>
+     *     Constructs a reactive function to dynamically render components.<br>
+     *     This is only recommended for complex methods, like if switches are required.<br>
+     *     If a simple condition is enough {@link #conditionalComponent(SerializableSupplier, String, Class, SerializableConsumer)} should be used instead!
+     * </p>
+     * <p>
+     *     The callback is updated whenever a signal used inside it is updated.
+     * </p>
+     *
+     * @param reactiveFunction The function to run on signal updates.
+     * @return This builder for chaining.
+     */
+    WindowBuilder reactive(SerializableFunction<ReactiveRenderBuilder, ReactiveRenderBuilder.ReactiveResult> reactiveFunction);
+
+    /**
+     * <p>
+     *     Renders the specified component whenever the condition is met.<br>
+     *     Any signal used inside the condition will cause it to update when the signal is updated.
+     * </p>
+     * <p>
+     *     The specified component is constructed upon invocation of this method and simply rendered/removed whenever the condition changes.<br>
+     *     Further updates to the components need to be handled by using signals.
+     * </p>
+     *
+     * @param condition       The condition that is reactive to signals used inside it.
+     * @param id              The id of the component to render.
+     * @param builderType     The type of builder to use.
+     * @param builderConsumer The consumer to configure the builder.
+     * @param <B>             The type of the Component Builder
+     * @return This builder for chaining
+     */
+    <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder conditionalComponent(SerializableSupplier<Boolean> condition, String id, Class<B> builderType, SerializableConsumer<B> builderConsumer);
+
+    <BV extends ComponentBuilder<? extends Component, Component>, BI extends ComponentBuilder<? extends Component, Component>> WindowBuilder renderWhenElse(SerializableSupplier<Boolean> condition, Class<BV> builderValidType, Consumer<BV> builderValidConsumer, Class<BI> builderInvalidType, SerializableConsumer<BI> builderInvalidConsumer);
+
+    /**
+     * <p>
+     *     Renders the specified component with the given id.
+     * </p>
+     *
+     * @param id              The id of the component to render
+     * @param builderType     The type of the builder to use
+     * @param builderConsumer The consumer to configure the builder
+     * @param <B>             The type of the component builder
+     * @return This Builder for chaining
+     */
+    <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder component(String id, Class<B> builderType, SerializableConsumer<B> builderConsumer);
+
+    /**
+     * Renders the specified component at the given position.
+     * This is useful if the position wasn't specified by a config file
+     *
+     * @param position
+     * @param id
+     * @param builderType
+     * @param builderConsumer
+     * @return
+     * @param <B>
+     */
+    <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder component(Position position, String id, Class<B> builderType, SerializableConsumer<B> builderConsumer);
+
+    <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder component(Position position, Class<B> builderType, SerializableConsumer<B> builderConsumer);
 
     Window create(Router parent);
 
--- common/src/main/java/com/wolfyscript/utilities/gui/WindowBuilderImpl.java
+++ common/src/main/java/com/wolfyscript/utilities/gui/WindowBuilderImpl.java
@@ -12,8 +12,12 @@ import com.wolfyscript.utilities.WolfyUtils;
 import com.wolfyscript.utilities.config.jackson.KeyedBaseType;
 import com.wolfyscript.utilities.gui.callback.InteractionCallback;
 import com.wolfyscript.utilities.gui.functions.SerializableConsumer;
-import com.wolfyscript.utilities.registry.RegistryGUIComponentBuilders;
+import com.wolfyscript.utilities.gui.functions.SerializableFunction;
+import com.wolfyscript.utilities.gui.functions.SerializableSupplier;
+import com.wolfyscript.utilities.gui.signal.Signal;
 import com.wolfyscript.utilities.tuple.Pair;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 
 import java.util.*;
 import java.util.function.Consumer;
@@ -27,17 +31,37 @@ public class WindowBuilderImpl implements WindowBuilder {
     private final WolfyUtils wolfyUtils;
     protected int size;
     protected WindowType type;
-    private String staticTitle = null;
+    private final ReactiveSource reactiveSource;
     private InteractionCallback interactionCallback = (guiHolder, interactionDetails) -> InteractionResult.def();
-    private Consumer<WindowDynamicConstructor> rendererConstructor = builder -> {};
-    private final Map<ComponentBuilder<?, ?>, Position> componentBuilderPositions = new HashMap<>();
+
+    /**
+     * Components
+     */
+    private final Map<String, Long> componentIdAliases = new HashMap<>();
+    private final Map<Long, ComponentBuilder<?, ?>> componentBuilderMap = new HashMap<>();
+    final Set<ComponentBuilder<?, ?>> componentRenderSet = new HashSet<>();
+
+    /**
+     * Tasks
+     */
+    protected List<Pair<Runnable, Long>> intervalRunnables = new ArrayList<>();
+
+    /**
+     * Title data
+     */
+    private final List<TagResolver> titleTagResolvers = new ArrayList<>();
+    private String staticTitle = null;
+    private SerializableSupplier<net.kyori.adventure.text.Component> titleFunction;
+    private final Set<com.wolfyscript.utilities.gui.signal.Signal<?>> titleSignals = new HashSet<>();
 
     @Inject
     @JsonCreator
     protected WindowBuilderImpl(@JsonProperty("id") String windowID,
-                                @JacksonInject("wolfyUtils") WolfyUtils wolfyUtils) {
+                                @JacksonInject("wolfyUtils") WolfyUtils wolfyUtils,
+                                @JacksonInject("reactiveSrc") ReactiveSource reactiveSource) {
         this.id = windowID;
         this.wolfyUtils = wolfyUtils;
+        this.reactiveSource = reactiveSource;
     }
 
     @JsonSetter("size")
@@ -77,7 +101,10 @@ public class WindowBuilderImpl implements WindowBuilder {
     @JsonSetter("placement")
     private void setPlacement(List<ComponentBuilder<?, ?>> componentBuilders) {
         for (ComponentBuilder<?, ?> componentBuilder : componentBuilders) {
-            componentBuilderPositions.put(componentBuilder, componentBuilder.position());
+            long numericId = ComponentUtil.nextId();
+            componentIdAliases.put(componentBuilder.id(), numericId);
+            wolfyUtils.getLogger().info("Load component builder from config: " + componentBuilder.id() + "  (" + numericId + ")");
+            componentBuilderMap.put(numericId, componentBuilder);
         }
     }
 
@@ -89,23 +116,199 @@ public class WindowBuilderImpl implements WindowBuilder {
     }
 
     @Override
-    public WindowBuilder construct(Consumer<WindowDynamicConstructor> render) {
-        Preconditions.checkNotNull(render);
-        this.rendererConstructor = render;
+    public WindowBuilder title(SerializableSupplier<net.kyori.adventure.text.Component> titleSupplier) {
+        this.titleFunction = titleSupplier;
         return this;
     }
 
     @Override
+    public WindowBuilder titleSignals(Signal<?>... signals) {
+        titleTagResolvers.addAll(Arrays.stream(signals).map(signal -> TagResolver.resolver(signal.tagName(), (argumentQueue, context) -> Tag.inserting(net.kyori.adventure.text.Component.text(String.valueOf(signal.get()))))).toList());
+        titleSignals.addAll(Arrays.stream(signals).toList());
+        return this;
+    }
+
+    @Override
+    public WindowBuilder addIntervalTask(Runnable runnable, long l) {
+        this.intervalRunnables.add(new Pair<>(runnable, l));
+        return this;
+    }
+
+    @Override
+    public WindowBuilder reactive(SerializableFunction<ReactiveRenderBuilder, ReactiveRenderBuilder.ReactiveResult> consumer) {
+        Effect effect = new Effect() {
+
+            private Component previousComponent = null;
+
+            @Override
+            public void update(ViewRuntime guiViewManager, GuiHolder guiHolder, RenderContext context) {
+                ReactiveRenderBuilderImpl builder = new ReactiveRenderBuilderImpl(wolfyUtils, new HashMap<>() /* TODO: ((WindowImpl) guiHolder.getCurrentWindow()).nonRenderedComponents */);
+                ReactiveRenderBuilder.ReactiveResult result = consumer.apply(builder);
+                Component component = result == null ? null : result.construct().construct(guiHolder, guiViewManager);
+                if (Objects.equals(previousComponent, component)) return;
+
+                if (previousComponent != null) {
+                    previousComponent.remove(guiHolder, guiViewManager, context);
+                }
+
+                previousComponent = component;
+                if (component == null) {
+                    return;
+                }
+
+                context.enterNode(component);
+                component.executeForAllSlots(component.offset() + component.position().slot(), internalSlot -> ((ViewRuntimeImpl) guiHolder.getViewManager()).updateLeaveNodes(component, internalSlot));
+                if (component instanceof Effect signalledObject) {
+                    signalledObject.update(guiViewManager, guiHolder, context);
+                }
+                context.exitNode();
+            }
+        };
+        for (com.wolfyscript.utilities.gui.signal.Signal<?> signal : consumer.getSignalsUsed()) {
+            signal.linkTo(effect);
+        }
+        return this;
+    }
+
+    @Override
+    public <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder conditionalComponent(SerializableSupplier<Boolean> condition, String id, Class<B> builderType, SerializableConsumer<B> builderConsumer) {
+        Pair<NamespacedKey, Class<B>> builderTypeInfo = ComponentUtil.getBuilderType(wolfyUtils, id, builderType);
+        B builder = findExistingComponentBuilder(id, builderTypeInfo.getValue(), builderTypeInfo.getKey())
+                .orElseThrow(() -> new IllegalStateException(String.format("Failed to link to component '%s'! Cannot find existing placement", id)));
+        builderConsumer.accept(builder);
+
+        Component component = builder.create(null);
+
+        Effect effect = new Effect() {
+
+            private final Map<Long, Boolean> previousResult = new HashMap<>();
+
+            @Override
+            public void update(ViewRuntime guiViewManager, GuiHolder guiHolder, RenderContext context) {
+                boolean result = condition.get();
+                if (result != previousResult.getOrDefault(guiViewManager.id(), false)) {
+                    previousResult.put(guiViewManager.id(), result);
+                    if (result) {
+                        context.enterNode(component);
+                        if (component.construct(guiHolder, guiViewManager) instanceof Effect signalledObject) {
+                            signalledObject.update(guiViewManager, guiHolder, context);
+                        }
+                        component.executeForAllSlots(component.offset() + component.position().slot(), slot2 -> ((ViewRuntimeImpl) guiHolder.getViewManager()).updateLeaveNodes(component, slot2));
+                        context.exitNode();
+                    } else {
+                        component.executeForAllSlots(component.offset() + component.position().slot(), slot2 -> {
+                            context.setStack(slot2, null);
+                            ((ViewRuntimeImpl) guiHolder.getViewManager()).updateLeaveNodes(null, slot2);
+                        });
+                    }
+                }
+            }
+        };
+
+        for (com.wolfyscript.utilities.gui.signal.Signal<?> signal : condition.getSignalsUsed()) {
+            signal.linkTo(effect);
+        }
+        return this;
+    }
+
+    @Override
+    public <BV extends ComponentBuilder<? extends Component, Component>, BI extends ComponentBuilder<? extends Component, Component>> WindowBuilder renderWhenElse(SerializableSupplier<Boolean> serializableSupplier, Class<BV> validBuilderType, Consumer<BV> validBuilder, Class<BI> invalidBuilderType, SerializableConsumer<BI> invalidBuilder) {
+        return null;
+    }
+
+    @Override
+    public <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder component(String id, Class<B> builderType, SerializableConsumer<B> builderConsumer) {
+        Pair<NamespacedKey, Class<B>> builderTypeInfo = ComponentUtil.getBuilderType(wolfyUtils, id, builderType);
+        B builder = findExistingComponentBuilder(id, builderTypeInfo.getValue(), builderTypeInfo.getKey()).orElseThrow(() -> new IllegalStateException(String.format("Failed to link to component '%s'! Cannot find existing placement", id)));
+        Injector injector = Guice.createInjector(Stage.PRODUCTION, binder -> {
+            binder.bind(WolfyUtils.class).toInstance(wolfyUtils);
+            binder.bind(String.class).toInstance(id);
+        });
+        injector.injectMembers(builder);
+        builderConsumer.accept(builder);
+        componentRenderSet.add(builder);
+        return this;
+    }
+
+    @Override
+    public <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder component(Position position, String id, Class<B> builderType, SerializableConsumer<B> builderConsumer) {
+        Pair<NamespacedKey, Class<B>> builderTypeInfo = ComponentUtil.getBuilderType(wolfyUtils, id, builderType);
+        findExistingComponentBuilder(id, builderTypeInfo.getValue(), builderTypeInfo.getKey()).ifPresentOrElse(builderConsumer, () -> {
+            B builder = instantiateNewBuilder(id, position, builderTypeInfo);
+            builderConsumer.accept(builder);
+            componentRenderSet.add(builder);
+        });
+        return this;
+    }
+
+    @Override
+    public <B extends ComponentBuilder<? extends Component, Component>> WindowBuilder component(Position position, Class<B> builderType, SerializableConsumer<B> builderConsumer) {
+        Pair<NamespacedKey, Class<B>> builderTypeInfo = ComponentUtil.getBuilderType(wolfyUtils, id, builderType);
+        B builder = instantiateNewBuilder(id, position, builderTypeInfo);
+        builderConsumer.accept(builder);
+        componentRenderSet.add(builder);
+        return this;
+    }
+
+    private <B extends ComponentBuilder<? extends Component, Component>> B instantiateNewBuilder(String id, Position position, Pair<NamespacedKey, Class<B>> builderTypeInfo) {
+        long numericId = ComponentUtil.nextId();
+        componentIdAliases.put(id, numericId);
+
+        Injector injector = Guice.createInjector(Stage.PRODUCTION, binder -> {
+            binder.bind(WolfyUtils.class).toInstance(wolfyUtils);
+            binder.bind(Long.class).toInstance(numericId);
+            binder.bind(Position.class).toInstance(position);
+            binder.bind(ReactiveSource.class).toInstance(reactiveSource);
+        });
+
+        B builder = injector.getInstance(builderTypeInfo.getValue());
+        componentBuilderMap.put(numericId, builder);
+        return builder;
+    }
+
+    private <B extends ComponentBuilder<? extends Component, Component>> Optional<B> findExistingComponentBuilder(String id, Class<B> builderImplType, NamespacedKey builderKey) {
+        if (!componentIdAliases.containsKey(id)) return Optional.empty(); // If there is no alias yet, then it wasn't created yet!
+        return findExistingComponentBuilder(componentIdAliases.get(id), builderImplType, builderKey);
+    }
+
+    private <B extends ComponentBuilder<? extends Component, Component>> Optional<B> findExistingComponentBuilder(long id, Class<B> builderImplType, NamespacedKey builderKey) {
+        ComponentBuilder<?, ?> componentBuilder = componentBuilderMap.get(id);
+        if (componentBuilder == null) {
+            return Optional.empty();
+        }
+        if (!componentBuilder.getType().equals(builderKey)) {
+            return Optional.empty();
+        }
+        return Optional.of(builderImplType.cast(componentBuilder));
+    }
+
+    @Override
     public Window create(Router parent) {
+
+        if (titleFunction == null && !titleTagResolvers.isEmpty()) {
+            titleFunction = () -> wolfyUtils.getChat().getMiniMessage().deserialize(staticTitle, TagResolver.resolver(titleTagResolvers));
+        }
+        if (titleFunction != null) {
+            Effect signalledObject = (viewManager, guiHolder, renderContext) -> renderContext.updateTitle(guiHolder, titleFunction.get());
+            for (Signal<?> signal : titleFunction.getSignalsUsed()) {
+                signal.linkTo(signalledObject);
+            }
+            for (Signal<?> signal : titleSignals) {
+                signal.linkTo(signalledObject);
+            }
+        }
+
+        var components = componentRenderSet.stream().map(componentBuilder -> (Component) componentBuilder.create(null)).toList();
+
         return new WindowImpl(
                 parent.getID() + "/" + id,
                 parent,
                 size,
                 type,
                 staticTitle,
+                titleFunction,
                 interactionCallback,
-                componentBuilderPositions,
-                rendererConstructor
+                components
         );
     }
 
