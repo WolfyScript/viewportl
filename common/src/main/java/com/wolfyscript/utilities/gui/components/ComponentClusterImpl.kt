298186df8fdd469e589b638a840e0025fa22dc1c
WolfyScript <wolfyscript@gmail.com>
2024-03-17 15:50:28 +0100

    Convert ComponentClusterImpl to kotlin

--- common/src/main/java/com/wolfyscript/utilities/gui/components/ComponentClusterImpl.kt
+++ common/src/main/java/com/wolfyscript/utilities/gui/components/ComponentClusterImpl.kt
@@ -1,71 +1,60 @@
-package com.wolfyscript.utilities.gui.components;
+package com.wolfyscript.utilities.gui.components
 
-import com.wolfyscript.utilities.KeyedStaticId;
-import com.wolfyscript.utilities.WolfyUtils;
-import com.wolfyscript.utilities.gui.*;
-import com.wolfyscript.utilities.gui.rendering.PropertyPosition;
-import com.wolfyscript.utilities.gui.rendering.RenderProperties;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import com.wolfyscript.utilities.KeyedStaticId
+import com.wolfyscript.utilities.WolfyUtils
+import com.wolfyscript.utilities.gui.Component
+import com.wolfyscript.utilities.gui.Renderable
+import com.wolfyscript.utilities.gui.ViewRuntimeImpl
+import com.wolfyscript.utilities.gui.rendering.RenderProperties
+import java.util.*
+import kotlin.math.abs
 
 @KeyedStaticId(key = "cluster")
-public class ComponentClusterImpl extends AbstractComponentImpl implements ComponentCluster {
+class ComponentClusterImpl(
+    internalID: String,
+    wolfyUtils: WolfyUtils,
+    parent: Component?,
+    properties: RenderProperties,
+    private val children: List<Component>
+) : AbstractComponentImpl(
+    internalID, wolfyUtils, parent, properties
+), ComponentCluster {
+    private val width: Int
+    private val height: Int
 
-    private int width;
-    private int height;
-    private final List<Component> children;
-
-    public ComponentClusterImpl(String internalID, WolfyUtils wolfyUtils, Component parent, RenderProperties properties, List<Component> children) {
-        super(internalID, wolfyUtils, parent, properties);
-        this.children = children;
-
-        int topLeft = 54;
-        this.width = 1;
-        this.height = Math.abs((topLeft / 9)) + 1;
+    init {
+        val topLeft = 54
+        this.width = 1
+        this.height = (abs((topLeft / 9).toDouble()) + 1).toInt()
     }
 
-    public ComponentClusterImpl(ComponentClusterImpl staticComponent) {
-        super(staticComponent.getID(), staticComponent.getWolfyUtils(), staticComponent.parent(), staticComponent.properties());
-        this.children = staticComponent.children;
+    override fun childComponents(): Set<Component> {
+        return HashSet(children)
     }
 
-    @Override
-    public Set<? extends Component> childComponents() {
-        return new HashSet<>(children);
+    override fun getChild(id: String): Optional<out Component> {
+        return Optional.empty()
     }
 
-    @Override
-    public Optional<? extends Component> getChild(String id) {
-        return Optional.empty();
+    override fun width(): Int {
+        return width
     }
 
-    @Override
-    public int width() {
-        return width;
+    override fun height(): Int {
+        return height
     }
 
-    @Override
-    public int height() {
-        return height;
+    override fun remove(viewRuntimeImpl: ViewRuntimeImpl, nodeId: Long, parentNode: Long) {
+        viewRuntimeImpl.renderingGraph.removeNode(nodeId)
     }
 
-    @Override
-    public void remove(@NotNull ViewRuntimeImpl viewRuntimeImpl, long nodeId, long parentNode) {
-        viewRuntimeImpl.getRenderingGraph().removeNode(nodeId);
-    }
+    override fun insert(viewRuntimeImpl: ViewRuntimeImpl, parentNode: Long) {
+        val id = viewRuntimeImpl.renderingGraph.addNode(this)
+        viewRuntimeImpl.renderingGraph.insertNodeChild(id, parentNode)
 
-    @Override
-    public void insert(@NotNull ViewRuntimeImpl viewRuntimeImpl, long parentNode) {
-        long id = viewRuntimeImpl.getRenderingGraph().addNode(this);
-        viewRuntimeImpl.getRenderingGraph().insertNodeChild(id, parentNode);
-
-        for (Component child : children) {
-            if (child instanceof Renderable renderable) {
-                renderable.insert(viewRuntimeImpl, id);
+        for (child in children) {
+            if (child is Renderable) {
+                child.insert(viewRuntimeImpl, id)
             }
         }
     }
