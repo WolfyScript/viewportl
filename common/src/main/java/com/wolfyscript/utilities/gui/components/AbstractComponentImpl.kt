399a50c201d7f5a14bb1e5816fbb5ec3148d0068
WolfyScript <wolfyscript@gmail.com>
2024-03-17 15:49:18 +0100

    Rename parameter of equals to super parameter name

--- common/src/main/java/com/wolfyscript/utilities/gui/components/AbstractComponentImpl.kt
+++ common/src/main/java/com/wolfyscript/utilities/gui/components/AbstractComponentImpl.kt
@@ -86,10 +86,10 @@ abstract class AbstractComponentImpl(
         return properties
     }
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as AbstractComponentImpl
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as AbstractComponentImpl
         return type == that.type && internalID == that.internalID
     }
 
