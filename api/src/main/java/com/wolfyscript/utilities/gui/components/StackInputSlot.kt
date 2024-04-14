b8c440e4df921a71c7ac3ddc938cd3467bf0c52f
WolfyScript <wolfyscript@gmail.com>
2024-04-14 22:30:16 +0200

    Make value a variable and onValueChange a property of interface

--- api/src/main/java/com/wolfyscript/utilities/gui/components/StackInputSlot.kt
+++ api/src/main/java/com/wolfyscript/utilities/gui/components/StackInputSlot.kt
@@ -20,11 +20,12 @@ package com.wolfyscript.utilities.gui.components
 import com.wolfyscript.utilities.gui.Component
 import com.wolfyscript.utilities.gui.interaction.Interactable
 import com.wolfyscript.utilities.platform.adapters.ItemStack
+import java.util.function.Consumer
 
 interface StackInputSlot : Component, Interactable {
 
-    fun value(): ItemStack?
+    var value: ItemStack?
 
-    fun value(stack: ItemStack?)
+    val onValueChange: Consumer<ItemStack?>?
 
 }
--- common/src/main/java/com/wolfyscript/utilities/gui/components/StackInputSlotImpl.kt
+++ common/src/main/java/com/wolfyscript/utilities/gui/components/StackInputSlotImpl.kt
@@ -16,27 +16,13 @@ class StackInputSlotImpl(
     internalID: String,
     wolfyUtils: WolfyUtils,
     parent: Component?,
-    private val onValueChange: Consumer<ItemStack?>?,
+    override val onValueChange: Consumer<ItemStack?>?,
     private val interactionCallback: InteractionCallback,
-    valueSupplier: Supplier<ItemStack?>?,
     properties: RenderProperties?
 ) : AbstractComponentImpl(
     internalID, wolfyUtils, parent, properties!!
 ), Interactable, StackInputSlot {
-    private var value: ItemStack? = null
-
-    init {
-        // TODO
-        //            if (details instanceof ClickInteractionDetailsImpl clickInteractionDetails) {
-        //                InventoryClickEvent event = clickInteractionDetails.getClickEvent();
-        //                InteractionUtils.applyItemFromInteractionEvent(event.getSlot(), event, Set.of(), itemStack -> {
-        //                    onValueChange.accept(new ItemStackImpl((WolfyUtilsBukkit) holder.getViewManager().getWolfyUtils(), itemStack));
-        //                });
-        //            }
-        if (valueSupplier != null) {
-            this.value = valueSupplier.get()
-        }
-    }
+    override var value: ItemStack? = null
 
     override fun width(): Int {
         return 1
@@ -50,10 +36,6 @@ class StackInputSlotImpl(
         return interactionCallback
     }
 
-    override fun value(): ItemStack? {
-        return value
-    }
-
     override fun remove(viewRuntimeImpl: ViewRuntimeImpl, nodeId: Long, parentNode: Long) {
         viewRuntimeImpl.renderingGraph.removeNode(nodeId)
     }
@@ -61,10 +43,5 @@ class StackInputSlotImpl(
     override fun insert(viewRuntimeImpl: ViewRuntimeImpl, parentNode: Long) {
         val id = viewRuntimeImpl.renderingGraph.addNode(this)
         viewRuntimeImpl.renderingGraph.insertNodeChild(id, parentNode)
-    }
-
-    override fun value(stack: ItemStack?) {
-        this.value = stack
-        onValueChange?.accept(stack)
     }
 }
