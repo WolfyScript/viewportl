c85fb3f626083c72c1ffc684b47031e4ef3faa47
WolfyScript <wolfyscript@gmail.com>
2024-08-19 19:34:25 +0200

    Update license

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,7 @@
 
 /*
- *       WolfyUtilities, APIs and Utilities for Minecraft Spigot plugins
- *                      Copyright (C) 2021  WolfyScript
+ *     viewportl - multiplatform GUI framework to easily create reactive GUIs
+ *     Copyright (C) 2024  WolfyScript
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
