33fb4ab88c10d5d02f4c2871952ca525ddcb1cad
WolfyScript <wolfyscript@gmail.com>
2024-09-01 20:40:24 +0200

    Implement interval api for components

--- common/src/main/java/com/wolfyscript/viewportl/common/gui/components/ComponentScopeImpl.kt
+++ common/src/main/java/com/wolfyscript/viewportl/common/gui/components/ComponentScopeImpl.kt
@@ -4,11 +4,21 @@ import com.wolfyscript.viewportl.common.gui.ViewRuntimeImpl
 import com.wolfyscript.viewportl.gui.components.ComponentScope
 import com.wolfyscript.viewportl.gui.reactivity.*
 
-class ComponentScopeImpl(runtimeImpl: ViewRuntimeImpl) : ComponentScope, ReactiveSource by runtimeImpl.reactiveSource {
+class ComponentScopeImpl(val runtime: ViewRuntimeImpl) : ComponentScope, ReactiveSource by runtime.reactiveSource {
 
     override fun interval(intervalInTicks: Long, runnable: Runnable) {
-        // TODO
-    }
+        val task = runtime.viewportl.scafall.scheduler.task(runtime.viewportl.scafall.corePlugin)
+            .interval(intervalInTicks)
+            .delay(1)
+            .execute {
+                runnable.run()
+                runtime.reactiveSource.runEffects()
+            }
+            .build()
 
+        createCleanup {
+            task.cancel()
+        }
+    }
 
 }
