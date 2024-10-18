913f175bafc0768f981b74d307c6dbf817b3ef81
WolfyScript <wolfyscript@gmail.com>
2024-10-18 19:53:19 +0200

    Use map instead of match in spigot ClickInfoExt

--- spigot/src/main/kotlin/com/wolfyscript/viewportl/spigot/gui/inventoryui/interaction/ClickInfoExt.kt
+++ spigot/src/main/kotlin/com/wolfyscript/viewportl/spigot/gui/inventoryui/interaction/ClickInfoExt.kt
@@ -24,47 +24,76 @@ import com.wolfyscript.viewportl.gui.interaction.ClickInfo
 import com.wolfyscript.viewportl.gui.interaction.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 
-fun InventoryClickEvent.convert() : ClickInfo {
+fun InventoryClickEvent.convert(): ClickInfo {
     val cursorStack = cursor.wrap()
     val slotStack = currentItem?.wrap()
 
     return when (click) {
         // dropping items
         org.bukkit.event.inventory.ClickType.DROP -> CommonClickInfoImpl.Drop.Single(rawSlot, cursorStack, slotStack)
-        org.bukkit.event.inventory.ClickType.CONTROL_DROP -> CommonClickInfoImpl.Drop.Full(rawSlot, cursorStack, slotStack)
-        org.bukkit.event.inventory.ClickType.WINDOW_BORDER_LEFT -> CommonClickInfoImpl.Drop.Outside.Primary(rawSlot, cursorStack, slotStack)
-        org.bukkit.event.inventory.ClickType.WINDOW_BORDER_RIGHT -> CommonClickInfoImpl.Drop.Outside.Secondary(rawSlot, cursorStack, slotStack)
+        org.bukkit.event.inventory.ClickType.CONTROL_DROP -> CommonClickInfoImpl.Drop.Full(
+            rawSlot,
+            cursorStack,
+            slotStack
+        )
+
+        org.bukkit.event.inventory.ClickType.WINDOW_BORDER_LEFT -> CommonClickInfoImpl.Drop.Outside.Primary(
+            rawSlot,
+            cursorStack,
+            slotStack
+        )
+
+        org.bukkit.event.inventory.ClickType.WINDOW_BORDER_RIGHT -> CommonClickInfoImpl.Drop.Outside.Secondary(
+            rawSlot,
+            cursorStack,
+            slotStack
+        )
         // Key clicks
-        org.bukkit.event.inventory.ClickType.NUMBER_KEY -> CommonClickInfoImpl.NumberPress(rawSlot, hotbarButton, cursorStack, slotStack)
+        org.bukkit.event.inventory.ClickType.NUMBER_KEY -> CommonClickInfoImpl.NumberPress(
+            rawSlot,
+            hotbarButton,
+            cursorStack,
+            slotStack
+        )
         // most common clicks
         org.bukkit.event.inventory.ClickType.LEFT -> CommonClickInfoImpl.Primary(rawSlot, cursorStack, slotStack)
         org.bukkit.event.inventory.ClickType.RIGHT -> CommonClickInfoImpl.Secondary(rawSlot, cursorStack, slotStack)
         org.bukkit.event.inventory.ClickType.MIDDLE -> CommonClickInfoImpl.Middle(rawSlot, cursorStack, slotStack)
         org.bukkit.event.inventory.ClickType.DOUBLE_CLICK -> CommonClickInfoImpl.Double(rawSlot, cursorStack, slotStack)
         // Shift
-        org.bukkit.event.inventory.ClickType.SHIFT_LEFT -> CommonClickInfoImpl.Shift.Primary(rawSlot, cursorStack, slotStack)
-        org.bukkit.event.inventory.ClickType.SHIFT_RIGHT -> CommonClickInfoImpl.Shift.Secondary(rawSlot, cursorStack, slotStack)
+        org.bukkit.event.inventory.ClickType.SHIFT_LEFT -> CommonClickInfoImpl.Shift.Primary(
+            rawSlot,
+            cursorStack,
+            slotStack
+        )
+
+        org.bukkit.event.inventory.ClickType.SHIFT_RIGHT -> CommonClickInfoImpl.Shift.Secondary(
+            rawSlot,
+            cursorStack,
+            slotStack
+        )
         // Creative
         org.bukkit.event.inventory.ClickType.CREATIVE -> CommonClickInfoImpl.Primary(rawSlot, cursorStack, slotStack)
         else -> throw IllegalStateException("Unexpected value: $click")
     }
 }
 
-fun InventoryClickEvent.wrapClickType() : ClickType {
-    return when (click) {
-        org.bukkit.event.inventory.ClickType.DROP -> ClickType.DROP
-        org.bukkit.event.inventory.ClickType.CONTROL_DROP -> ClickType.CONTROL_DROP
-        org.bukkit.event.inventory.ClickType.LEFT -> ClickType.PRIMARY
-        org.bukkit.event.inventory.ClickType.RIGHT -> ClickType.SECONDARY
-        org.bukkit.event.inventory.ClickType.SHIFT_LEFT -> ClickType.SHIFT_PRIMARY
-        org.bukkit.event.inventory.ClickType.SHIFT_RIGHT -> ClickType.SHIFT_SECONDARY
-        org.bukkit.event.inventory.ClickType.MIDDLE -> ClickType.MIDDLE
-        org.bukkit.event.inventory.ClickType.CREATIVE -> ClickType.CREATIVE
-        org.bukkit.event.inventory.ClickType.NUMBER_KEY -> ClickType.NUMBER_KEY
-        org.bukkit.event.inventory.ClickType.DOUBLE_CLICK -> ClickType.DOUBLE_CLICK
-        org.bukkit.event.inventory.ClickType.WINDOW_BORDER_LEFT -> ClickType.CONTAINER_BORDER_PRIMARY
-        org.bukkit.event.inventory.ClickType.WINDOW_BORDER_RIGHT -> ClickType.CONTAINER_BORDER_SECONDARY
-        else -> throw IllegalStateException("Unexpected value: $click")
-    }
+private val spigotClickTypeToViewportl: Map<org.bukkit.event.inventory.ClickType, ClickType> = mapOf(
+    org.bukkit.event.inventory.ClickType.DROP to ClickType.DROP,
+    org.bukkit.event.inventory.ClickType.CONTROL_DROP to ClickType.CONTROL_DROP,
+    org.bukkit.event.inventory.ClickType.LEFT to ClickType.PRIMARY,
+    org.bukkit.event.inventory.ClickType.RIGHT to ClickType.SECONDARY,
+    org.bukkit.event.inventory.ClickType.SHIFT_LEFT to ClickType.SHIFT_PRIMARY,
+    org.bukkit.event.inventory.ClickType.SHIFT_RIGHT to ClickType.SHIFT_SECONDARY,
+    org.bukkit.event.inventory.ClickType.MIDDLE to ClickType.MIDDLE,
+    org.bukkit.event.inventory.ClickType.CREATIVE to ClickType.CREATIVE,
+    org.bukkit.event.inventory.ClickType.NUMBER_KEY to ClickType.NUMBER_KEY,
+    org.bukkit.event.inventory.ClickType.DOUBLE_CLICK to ClickType.DOUBLE_CLICK,
+    org.bukkit.event.inventory.ClickType.WINDOW_BORDER_LEFT to ClickType.CONTAINER_BORDER_PRIMARY,
+    org.bukkit.event.inventory.ClickType.WINDOW_BORDER_RIGHT to ClickType.CONTAINER_BORDER_SECONDARY,
+)
+
+fun InventoryClickEvent.wrapClickType(): ClickType {
+    return spigotClickTypeToViewportl[click] ?: throw IllegalStateException("Unexpected value: $click")
 }
 
