8f7dbba5542559dbbe966077a4d14bb1764c4cd7
WolfyScript <wolfyscript@gmail.com>
2025-11-05 22:32:19 +0100

    impl: add column layout logic

--- api/src/main/java/com/wolfyscript/viewportl/gui/elements/Column.kt
+++ api/src/main/java/com/wolfyscript/viewportl/gui/elements/Column.kt
@@ -1,13 +1,28 @@
 package com.wolfyscript.viewportl.gui.elements
 
 import androidx.compose.runtime.Composable
+import com.wolfyscript.viewportl.gui.compose.layout.Constraints
+import com.wolfyscript.viewportl.gui.compose.layout.dp
+import com.wolfyscript.viewportl.gui.compose.layout.or
+import com.wolfyscript.viewportl.gui.compose.layout.slots
 
 @Composable
 fun Column(content: @Composable () -> Unit) {
     Layout({}, content = content) { measurables, constraints ->
 
-        layout(constraints.maxWidth, constraints.maxHeight) {
+        // TODO: Alignment options
+        // TODO: adjust width based on children
+        val maxHeightPerChild = constraints.maxHeight / measurables.size
+        val childConstraints = Constraints(constraints.minWidth, constraints.maxWidth, constraints.minHeight, maxHeightPerChild)
 
+        val placeables = measurables.map { it.measure(childConstraints) }
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var offset = 0.slots or 0.dp
+            for (placeable in placeables) {
+                placeable.placeAt(0.slots or 0.dp, offset)
+                offset += maxHeightPerChild
+            }
         }
     }
 }
