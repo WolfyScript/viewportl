dc820b4359c960f04b4f6db4c288bae7605eb0d2
WolfyScript <wolfyscript@gmail.com>
2026-03-11 19:18:35 +0100

    feat: PlacementScope offset for child placements

--- api/src/main/java/com/wolfyscript/viewportl/ui/SimplePlacementScope.kt
+++ api/src/main/java/com/wolfyscript/viewportl/ui/SimplePlacementScope.kt
@@ -1,6 +1,10 @@
 package com.wolfyscript.viewportl.ui
 
+import com.wolfyscript.viewportl.ui.layout.Offset
 import com.wolfyscript.viewportl.ui.layout.PlacementScope
 
 class SimplePlacementScope : PlacementScope {
+
+    override var offset: Offset = Offset.Zero
+
 }
--- api/src/main/java/com/wolfyscript/viewportl/ui/layout/PlacementScope.kt
+++ api/src/main/java/com/wolfyscript/viewportl/ui/layout/PlacementScope.kt
@@ -1,4 +1,30 @@
 package com.wolfyscript.viewportl.ui.layout
 
 interface PlacementScope {
+
+    /**
+     * The offset that gets applied to all following [place] calls within this scope.
+     */
+    var offset: Offset
+
+    fun Placeable.place(x: Dp, y: Dp) {
+        this.placeNoOffset(x + offset.x, y + offset.y)
+    }
+
+    fun offsetY(y: Dp) {
+        offset = Offset(offset.x, offset.y + y)
+    }
+
+    fun offsetX(x: Dp) {
+        offset = Offset(offset.x + x, offset.y)
+    }
+
+    fun offset(offset: Offset) {
+        this.offset = offset.plus(offset)
+    }
+
+    fun resetOffset() {
+        offset = Offset.Zero
+    }
+
 }
