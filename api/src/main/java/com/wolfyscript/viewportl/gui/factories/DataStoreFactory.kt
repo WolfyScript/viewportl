9414dd9e9f13d9ec6cf2b3759607dab63ddfc532
WolfyScript <wolfyscript@gmail.com>
2026-04-17 18:32:34 +0200

    refactor: remove more unused code

--- api/src/main/java/com/wolfyscript/viewportl/gui/factories/DataStoreFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.wolfyscript.viewportl.gui.factories
-
-import com.wolfyscript.viewportl.gui.model.DataStoreMap
-
-interface DataStoreFactory {
-
-    fun createDataStoreMap(): DataStoreMap
-
-}
